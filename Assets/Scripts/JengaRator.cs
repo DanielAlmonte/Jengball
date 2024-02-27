be6a850e4015d7138ecf13e88f04c95c01717a64
Daniel Almopnte <d.almonte562003@gmail.com>
2024-02-27 11:19:15 +0100

    Jenga generator script & prefab

---
+++ Assets/Scripts/JengaRator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JengaRator : MonoBehaviour
+{
+    public GameObject JengaTower, JengaPiece, JengaRotatedPiece;
+    public int JengaTowerWidth = 3, JengaTowerHeight = 18;
+    public Material[] Materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateJengaTower();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateJengaTower()
+    {
+        Instantiate(JengaTower, new Vector3(0,0,0), Quaternion.identity);
+
+        // int JP = 1;
+
+        // int ArrSize = JengaTowerWidth * JengaTowerHeight/2;
+        
+        // GameObject[] JengaPiecesArr = new GameObject[ArrSize];
+        // GameObject[] JengaRotatedPiecesArr = new GameObject[ArrSize];
+
+        // for (var h = 0; h < (JengaTowerHeight/2); h++)
+        // {
+        //     for (var w = 0; w < JengaTowerWidth; w++)
+        //     {
+        //         JengaPiecesArr[w] = Instantiate(JengaPiece, new Vector3((w + (JengaTowerWidth/2f)), h*2, 1), Quaternion.identity);
+        //         JengaPiecesArr[w].name = ("JengaPiece" + " " + JP++);
+        //         JengaPiecesArr[w].tag = "JengaPiece";
+        //         // JengaPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+
+        //         JengaRotatedPiecesArr[w] = Instantiate(JengaRotatedPiece, new Vector3(2.5f, (h*2 + 1 ), (w * 1)), Quaternion.identity);
+        //         JengaRotatedPiecesArr[w].name = ("JengaPiece" + " " + JP++);
+        //         JengaRotatedPiecesArr[w].tag = "JengaPiece";
+        //         // JengaRotatedPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+        //     }
+        // }
+    }
+}
