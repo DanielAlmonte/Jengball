1aa4eefb469ecdaeaa480ca5b35bba62a4953dc0
Daniel Almopnte <d.almonte562003@gmail.com>
2024-02-28 09:30:43 +0100

    GameObject array changed to list + delete test

--- Assets/Scripts/JengaRator.cs
+++ Assets/Scripts/JengaRator.cs
@@ -17,14 +17,51 @@ public class JengaRator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RemoveTest();
     }
 
+    List<GameObject> JengaPiecesArr = new List<GameObject>();
+    List<GameObject> JengaRotatedPiecesArr = new List<GameObject>();
     void GenerateJengaTower()
     {
-        Instantiate(JengaTower, new Vector3(0,0,0), Quaternion.identity);
+        // Instantiate(JengaTower, new Vector3(0,0,0), Quaternion.identity);
 
         // int JP = 1;
+
+
+        for (var h = 0; h < (JengaTowerHeight/2); h++)
+        {
+            for (var w = 0; w < JengaTowerWidth; w++)
+            {
+                JengaPiecesArr.Add(Instantiate(JengaPiece, new Vector3((w + (JengaTowerWidth/2f)), h*2, 1), Quaternion.identity) as GameObject);
+                // JengaPiecesArr[w].name = ("JengaPiece" + " " + JP++);
+                // JengaPiecesArr[w].tag = "JengaPiece";
+
+                // Debug.Log(JengaPiecesArr[w].name);
+
+                // JengaPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+
+                JengaRotatedPiecesArr.Add(Instantiate(JengaRotatedPiece, new Vector3(2.5f, (h*2 + 1 ), (w * 1)), Quaternion.identity) as GameObject);
+                // JengaRotatedPiecesArr[w].name = ("JengaRotatedPiece" + " " + JP++);
+                // JengaRotatedPiecesArr[w].tag = "JengaPiece";
+
+                // Debug.Log(JengaRotatedPiecesArr[w].name);
+
+                // JengaRotatedPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+            }
+        }
+        
+        // for (var t = 0; t < 27; t++)
+        // {   JengaPiecesArr[t].name = ("JengaPiece" + " " + JP++);
+        //     Debug.Log(JengaPiecesArr[t]);
+
+        //     JengaRotatedPiecesArr[t].name = ("JengaRotatedPiece" + " " + JP++);
+        //     Debug.Log(JengaRotatedPiecesArr[t]);
+        // }
+
+
+
+
 
         // int ArrSize = JengaTowerWidth * JengaTowerHeight/2;
         
@@ -38,13 +75,34 @@ public class JengaRator : MonoBehaviour
         //         JengaPiecesArr[w] = Instantiate(JengaPiece, new Vector3((w + (JengaTowerWidth/2f)), h*2, 1), Quaternion.identity);
         //         JengaPiecesArr[w].name = ("JengaPiece" + " " + JP++);
         //         JengaPiecesArr[w].tag = "JengaPiece";
+
+        //         // Debug.Log(JengaPiecesArr[w].name);
+
         //         // JengaPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
 
         //         JengaRotatedPiecesArr[w] = Instantiate(JengaRotatedPiece, new Vector3(2.5f, (h*2 + 1 ), (w * 1)), Quaternion.identity);
         //         JengaRotatedPiecesArr[w].name = ("JengaPiece" + " " + JP++);
         //         JengaRotatedPiecesArr[w].tag = "JengaPiece";
+
+        //         // Debug.Log(JengaRotatedPiecesArr[w].name);
+
         //         // JengaRotatedPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
         //     }
+        // }
+    }
+
+    void RemoveTest()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Destroy(JengaPiecesArr[3]);
+        }
+        // for (var t = 0; t < 27; t++)
+        // {   JengaPiecesArr[t].name = ("JengaPiece" + " " + JP++);
+        //     Debug.Log(JengaPiecesArr[t]);
+
+        //     JengaRotatedPiecesArr[t].name = ("JengaRotatedPiece" + " " + JP++);
+        //     Debug.Log(JengaRotatedPiecesArr[t]);
         // }
     }
 }
