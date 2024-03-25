c624e27695fcf0666044132c64f05db35ebb156e
Hendrik <hjm.goodijk@student.alfa-college.nl>
2024-03-25 12:01:10 +0100

    lats

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,13 +7,44 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
+    //making piece in peace
+    public Piece selectedPiece = null;
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        SelectPiece();
     }
+
+    public void SelectPiece()
+    {
+        // Return if left or right mouse button is not pressed.
+        if (!Input.GetKeyDown(KeyCode.Mouse0)) return;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+        {
+            // Check if the raycast hits piece.
+            if (hit.collider.tag == "Piece")
+            {
+                if (selectedPiece != null)
+                {
+                    //deselectPiece doe iets
+                    selectedPiece.selected = false;
+
+/*                    Debug.Log("selected a piece");
+*/                }
+
+                selectedPiece = hit.collider.gameObject.GetComponent<Piece>();
+                selectedPiece.selected = true;
+            }
+        }
+    }
+
+
 }   
---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+// trying somehthing hereeeeeeeee
+// need to this at home
+
+public class RoundManager : MonoBehaviour
+{
+    public bool player1Turn = true;
+    public bool player2Turn = false;
+
+    [SerializeField] private Image player1Icon;
+    [SerializeField] private Image player2Icon;
+
+   
+
+    CameraController cameraController;
+    void Start()
+    {
+        cameraController = FindObjectOfType<CameraController>();
+        
+        player1Icon.color = Color.white;
+        player2Icon.color = Color.white;
+
+        
+    }
+
+    void Update()
+    {
+        CheckWhosTurn();
+    }
+
+       void CheckWhosTurn()
+    {
+        if(cameraController.selectedPiece == null) return;        
+        Debug.Log("piece is not null");
+
+        if (!cameraController.selectedPiece.isColliding) return;  
+        // player 1 aan de ganng
+        if(cameraController.selectedPiece.selected)
+        {
+            Debug.Log("player 1 turn");
+            player1Turn = true;
+            player2Turn = false;
+        }
+        else
+        {
+            Debug.Log("player 2 turn");
+            player1Turn = false;
+            player2Turn = true;
+        }        
+        ChangePlayerIcon();
+    }
+
+    void ChangePlayerIcon()
+    {
+        if(player1Turn) 
+        {
+            player1Icon.color = Color.red;
+            player2Icon.color = Color.white;
+        }
+        else
+        {
+            player2Icon.color = Color.green;
+            player1Icon.color = Color.white;
+        }
+    }
+
+ 
+}
