5f8669f50d8a8f86ea6d72511aec4712ae4ddbe1
Daniel Almopnte <d.almonte562003@gmail.com>
2024-03-25 20:48:53 +0100

    Game scene 3d models added & Jenga piece outer box

--- Assets/Scripts/JengaRator.cs
+++ Assets/Scripts/JengaRator.cs
@@ -31,11 +31,11 @@ public class JengaRator : MonoBehaviour
         {
             for (var w = 0; w < JengaTowerWidth; w++)
             {
-                JengaPiecesList.Add(Instantiate(JengaPiece, new Vector3(1, (h), w), Quaternion.identity) as GameObject);
-                // JengaPiecesList[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
-
-                JengaPiecesList.Add(Instantiate(JengaRotatedPiece, new Vector3(w, ((h) + 0.5f), 1), Quaternion.identity * Quaternion.Euler (0, 90, 0)) as GameObject);
+                JengaPiecesList.Add(Instantiate(JengaPiece, new Vector3(w, ((h) + 0.5f), 1), Quaternion.Euler (0f, 0f, 0f)) as GameObject);
                 // JengaRotatedPiecesArr[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+
+                JengaPiecesList.Add(Instantiate(JengaRotatedPiece, new Vector3(1, (h), w), Quaternion.Euler (0f, 90f, 0f)) as GameObject);
+                // JengaPiecesList[w].GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
             }
         }
         
